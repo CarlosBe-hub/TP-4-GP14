133d65399403b074e471508268d24d93f0efef2a
Agustin-Benenatti <agus.benenatti09@gmail.com>
2024-08-27 20:44:01 -0300

    Cambios
    Creacion de las clases materia y alumno

---
+++ src/tp/pkg4/gp14/Alumno.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.pkg4.gp14;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ *
+ * @author aguse
+ */
+public class Alumno {
+    private int nrolegajo;
+    private String apellido;
+    private String nombre;
+    private Set<Materia> materias;
+
+    public Alumno(int nrolegajo, String apellido, String nombre) {
+        this.nrolegajo = nrolegajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.materias = new HashSet<>();
+    }
+
+    public Alumno() {
+    }
+    
+
+    public int getNrolegajo() {
+        return nrolegajo;
+    }
+
+    public void setNrolegajo(int nrolegajo) {
+        this.nrolegajo = nrolegajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void agregarMaterias(Materia materia){
+        if(materias.contains(materia)){
+            System.out.println("El alumno ya esta inscripto en esta materia");
+        
+        }
+        else{
+            materias.add(materia);
+            System.out.println("Materia agregada: " +materia);
+        }
+    
+    }
+    
+    public int cantidadMaterias(){
+        return materias.size();
+        
+      
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "nrolegajo=" + nrolegajo + ", apellido=" + apellido + ", nombre=" + nombre + ", materias=" + materias + '}';
+    }
+    
+}
---
+++ src/tp/pkg4/gp14/Materia.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.pkg4.gp14;
+
+/**
+ *
+ * @author aguse
+ */
+public class Materia {
+    private int idmateria;
+    private String nombre;
+    private int año;
+
+    public Materia(int idmateria, String nombre, int año) {
+        this.idmateria = idmateria;
+        this.nombre = nombre;
+        this.año = año;
+        
+    }
+
+    public Materia() {
+    }
+
+    public int getIdmateria() {
+        return idmateria;
+    }
+
+    public void setIdmateria(int idmateria) {
+        this.idmateria = idmateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idmateria=" + idmateria + ", nombre=" + nombre + ", a\u00f1o=" + año + '}';
+    }
+    
+    
+}
